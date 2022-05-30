19088305adf7507d97109357156ad29c97860287
ingig <ingigauti@gmail.com>
2022-05-30 15:27:13 +0200

    add test and fix them

--- tests/contracts/LiminalMarketTests.ts
+++ tests/contracts/LiminalMarketTests.ts
@@ -2,19 +2,15 @@ import chai, { should } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import { solidity, MockProvider } from "ethereum-waffle";
 
-import ContractJson from "../artifacts/contracts/LiminalExchange.sol/LiminalExchange.json";
-import { LiminalExchange } from '../typechain-types/LiminalExchange';
-import { SecurityFactory } from '../typechain-types/SecurityFactory';
-import { Oracle } from '../typechain-types/Oracle';
-import { AUSD } from '../typechain-types/AUSD';
-import { KYC } from '../typechain-types/KYC';
+import { LiminalMarket } from '../../typechain-types/contracts/LiminalMarket';
+import { AUSD } from '../../typechain-types/contracts/AUSD';
+import { KYC } from '../../typechain-types/contracts/KYC';
 import { BigNumber, Wallet } from 'ethers';
 import { FakeContract, smock } from '@defi-wonderland/smock';
 import { text } from 'stream/consumers';
-import { ERC20 } from '../typechain-types/ERC20';
+import hre, {upgrades} from "hardhat";
 
-describe("LiminalExchange", function () {
-	const hre = require("hardhat");
+describe("LiminalMarket", function () {
 	const waffle = hre.waffle;
 	const { deployContract, deployMockContract } = hre.waffle;
 	const [wallet] = new MockProvider().getWallets()
@@ -26,18 +22,13 @@ describe("LiminalExchange", function () {
 	chai.use(smock.matchers);
 
 	let owner: Wallet, wallet2: Wallet, wallet3: Wallet;
-	let contract: LiminalExchange;
+	let contract: LiminalMarket;
 
-	let kycContract: any;
-	let securityFactory: any
-	let oracleContract: Oracle;
+	let kycContract: KYC;
 	let aUsdContract: AUSD;
-	let usdcContract: any;
 
 	const liminalWalletAddress = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8';
 	const brokerAddress = '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC';
-	const linkTokenAddress = '0x01BE23585060835E02B77ef475b0Cc51aA1e0709';
-	const usdcContractAddress = '0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b';
 
 	const accountId: string = "aee548b2-b250-449c-8d0b-937b0b87ccef";
 	const symbol = "TST";
@@ -50,51 +41,21 @@ describe("LiminalExchange", function () {
 	})
 
 	const redeployContract = async function () {
-		const securityFactoryFactory = await smock.mock('SecurityFactory');
-		securityFactory = await smock.fake('SecurityFactory'); //await securityFactoryFactory.deploy() as unknown as SecurityFactory;
 
 		const kycContractFactory = await smock.mock('KYC');
-		kycContract = await smock.fake('KYC'); // await kycContractFactory.deploy() as unknown as KYC;
+		kycContract = await smock.fake('KYC') as unknown as KYC; // await kycContractFactory.deploy() as unknown as KYC;
 
 		const aUsdContractFactory = await smock.mock('aUSD');
 		aUsdContract = await aUsdContractFactory.deploy() as unknown as AUSD;
 
-		const oracleContractFactory = await smock.mock('Oracle');
-		//oracleContract = await oracleContractFactory.deploy([linkTokenAddress]) as unknown as Oracle;
-
-
-		const usdcContractFactory = await smock.mock('ERC20');
-		usdcContract = await smock.fake('ERC20'); //await usdcContractFactory.deploy("USDC", "USDC2");
-
-
-		contract = await deployContract(owner, ContractJson,
-			[
-				securityFactory.address,
-				securityFactory.address, //should be oracleContract.address just while LINK hasnt been implemented
-				kycContract.address,
-				aUsdContract.address,
-				liminalWalletAddress,
-				brokerAddress,
-				linkTokenAddress,
-				usdcContract.address
-			]) as unknown as LiminalExchange;
-		contract = await contract.connect(owner);
+		const contractFactory = await hre.ethers.getContractFactory('LiminalMarket');
+		contract = await upgrades.deployProxy(contractFactory) as LiminalMarket;
 
 	}
 
 
-	it("calculate fee for $1000, since USDC has 6 decimal point it's 10**6", async function () {
-		await redeployContract();
-		await expect(await contract.callStatic.calculateFee(1000 * 10**6)).to.be.equal(5 * 10**6);
-	});
-
-	it("calculate fee for $100, since USDC has 6 decimal point it's 10**6, since we expecct 0.5 it's 10**5", async function () {
-		await redeployContract();
-		await expect(await contract.callStatic.calculateFee(100 * 10**6)).to.be.equal(5 * 10**5);
-	});
-
 	it("buy but symbol is empty, throw error", async function () {
-		await expect(contract.buy("", 100)).to.be.reverted;
+		await expect(contract..buy("", 100)).to.be.reverted;
 	})
 
 	it("buy but amount is 0, throw error", async function () {
--- tests/contracts/aUsdTests.ts
+++ tests/contracts/aUsdTests.ts
@@ -1,22 +1,26 @@
 
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import { solidity } from "ethereum-waffle";
-
+import {MockContract, solidity} from "ethereum-waffle";
+import LiminalMarketJson from '../../artifacts/contracts/LiminalMarket.sol/LiminalMarket.json';
 import { Wallet } from 'ethers';
 import hre, {upgrades} from "hardhat";
-import {AUSD} from "../../typechain-types";
+import {AUSD, LiminalMarket} from "../../typechain-types";
+import { FakeContract, smock } from '@defi-wonderland/smock';
 
 
 describe("aUsd", function () {
   const expect = chai.expect;
   const hre = require("hardhat");
   const waffle = hre.waffle;
+  chai.should();
   chai.use(chaiAsPromised);
   chai.use(solidity);
+  chai.use(smock.matchers);
 
   let owner : Wallet, wallet2 : Wallet, wallet3 : Wallet;
   let contract: AUSD;
+  let liminalMarketContract : FakeContract<LiminalMarket>;
 
 
   before("before running each test", async function () {
@@ -28,11 +32,25 @@ describe("aUsd", function () {
   })
 
   const redeployContract = async function() {
-   // contract = await deployContract(owner, ContractJson);
-
     const contractFactory = await hre.ethers.getContractFactory('aUSD');
     contract = await upgrades.deployProxy(contractFactory) as AUSD;
   }
+
+  it('Call transfer method, should call buyWithAUsd on LiminalMarket contract', async function() {
+    let tokenAddress = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8';
+    let amount = 12;
+    let expectedResult = true;
+
+    liminalMarketContract = await smock.fake<LiminalMarket>('LiminalMarket');
+    await liminalMarketContract.buyWithAUsd.returns(expectedResult);
+
+    await contract.setLiminalMarketAddress(liminalMarketContract.address);
+
+    let contractW2 = contract.connect(wallet2);
+    await contractW2.transfer(tokenAddress, amount);
+
+    expect(liminalMarketContract.buyWithAUsd).to.have.been.calledWith(wallet2.address, tokenAddress, amount);
+  })
 
   it("Check balance of new deployment", async function () {
     let balanceOf = await contract.balanceOf(owner.address);
@@ -64,40 +82,60 @@ describe("aUsd", function () {
     await contract.grantRoleForBalance(wallet3.address);
     let amount = 235;
 
-    contract = await contract.connect(wallet3);
-    await contract.setBalance(wallet2.address, amount);
+    let contractW3 = contract.connect(wallet3);
+    await contractW3.setBalance(wallet2.address, amount);
     await expect(await contract.balanceOf(wallet2.address)).to.be.equal(amount);
   });
 
   it("Try to set balance with wallet that doesnt have permission, should fail", async function() {
-    contract = await contract.connect(wallet3);
-    await expect(contract.setBalance(wallet2.address, 199)).to.be.rejected;
+    await redeployContract();
+
+    let contractW3 = contract.connect(wallet3);
+    await expect(contractW3.setBalance(wallet2.address, 199)).to.be.rejected;
   });
 
   it("give role", async function() {
     await contract.grantRoleForBalance(wallet2.address);
   })
 
-
-
   it("Transfer should be rejected", async function () {
-
     expect(contract.transfer(owner.address, 10)).to.be.reverted;
   });
 
 
   it("allowance should be rejected", async function () {
-
     expect(contract.allowance(owner.address, wallet2.address)).to.be.reverted;
   })
 
   it("approve should be rejected", async function () {
-
     expect(contract.approve(owner.address, 10)).to.be.rejected;
   })
 
   it("transferFrom should be rejected", async function () {
-
     expect(contract.transferFrom(owner.address, wallet2.address, 10)).to.be.reverted;
   })
+
+
+  it("Pause contract, try to execute on it", async function () {
+    await contract.pause();
+    await expect(contract.transfer(wallet2.address, 12)).to.be.reverted;
+  });
+
+  it("Pause contract, try to execute on it, then unpause contract and execute again", async function () {
+    await redeployContract();
+
+    liminalMarketContract = await smock.fake<LiminalMarket>('LiminalMarket');
+    await liminalMarketContract.buyWithAUsd.returns(true);
+    await contract.setLiminalMarketAddress(liminalMarketContract.address);
+
+    await contract.pause();
+    await expect(contract.transfer(wallet2.address, 12)).to.be.reverted;
+
+    await contract.unpause();
+    let tokenAddress = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8';
+    let amount = 1244;
+    await contract.transfer(tokenAddress, amount);
+
+    expect(liminalMarketContract.buyWithAUsd).to.have.been.calledWith(owner.address, tokenAddress, amount);
+  });
 });
